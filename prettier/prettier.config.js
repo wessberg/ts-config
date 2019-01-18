e0d305d3a88be317688ccc58cacb9bc8ad8a363e
wessberg <frederikwessberg@gmail.com>
2019-01-18 23:00:20 +0100

    Added a prettier config and updated TSLint rules to turn off those rules that prettier are in control of

---
+++ prettier/prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 2,
+	singleQuote: false,
+	printWidth: 200,
+	bracketSpacing: false,
+	arrowParens: "avoid",
+	semi: true,
+	trailingComma: "none",
+	endOfLine: "lf"
+};
