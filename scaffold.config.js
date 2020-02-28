8145b018fc5590d0df4cedfad6cfb904725d92ed
wessberg <frederikwessberg@gmail.com>
2020-02-28 23:58:36 +0100

    chore: add sponsor

--- scaffold.config.js
+++ scaffold.config.js
@@ -1,5 +1,15 @@
 module.exports = {
 	donate: {
+		other: {
+			donors: [
+				{
+					name: "Bubbles",
+					url: "https://usebubbles.com",
+					twitter: "use_bubbles",
+					imageUrl: "https://uploads-ssl.webflow.com/5d682047c28b217055606673/5e5360be16879c1d0dca6514_icon-thin-128x128%402x.png"
+				}
+			]
+		},
 		patreon: {
 			userId: "11315442",
 			username: "wessberg"
