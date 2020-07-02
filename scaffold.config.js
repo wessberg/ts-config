050398d77ea38c7f485ba5d69109a5ed7a0db887
wessberg <frederikwessberg@gmail.com>
2020-07-02 11:40:12 +0200

    feat: add Christopher Blanchard as sponsor

--- scaffold.config.js
+++ scaffold.config.js
@@ -7,6 +7,11 @@ module.exports = {
 					url: "https://usebubbles.com",
 					twitter: "use_bubbles",
 					imageUrl: "https://uploads-ssl.webflow.com/5d682047c28b217055606673/5e5360be16879c1d0dca6514_icon-thin-128x128%402x.png"
+				},
+				{
+					name: "Christopher Blanchard",
+					url: "https://github.com/cblanc",
+					imageUrl: "https://avatars0.githubusercontent.com/u/2160685?s=400&v=4"
 				}
 			]
 		},
