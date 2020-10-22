a0ec767c92c6cdd8bf24efdc80b83744498055e3
wessberg <frederikwessberg@gmail.com>
2020-10-23 01:30:22 +0200

    update twitter handle for bubbles

--- scaffold.config.js
+++ scaffold.config.js
@@ -5,7 +5,7 @@ module.exports = {
 				{
 					name: "Bubbles",
 					url: "https://usebubbles.com",
-					twitter: "use_bubbles",
+					twitter: "usebubbles",
 					imageUrl: "https://uploads-ssl.webflow.com/5d682047c28b217055606673/5e5360be16879c1d0dca6514_icon-thin-128x128%402x.png"
 				},
 				{
