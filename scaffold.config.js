727679b02636cee92cff632adf04cf240695e326
Frederik Wessberg <frederikwessberg@gmail.com>
2021-03-16 20:04:08 +0100

    chore(sponsors): add new sponsors

--- scaffold.config.js
+++ scaffold.config.js
@@ -12,6 +12,16 @@ module.exports = {
 					name: "Christopher Blanchard",
 					url: "https://github.com/cblanc",
 					imageUrl: "https://avatars0.githubusercontent.com/u/2160685?s=400&v=4"
+				},
+				{
+					name: "Ideal Postcodes",
+					url: "https://github.com/ideal-postcodes",
+					imageUrl: "https://avatars.githubusercontent.com/u/4996310?s=200&v=4"
+				},
+				{
+					name: "Xerox",
+					url: "https://www.xerox.com",
+					imageUrl: "https://avatars.githubusercontent.com/u/9158512?s=200&v=4"
 				}
 			]
 		},
